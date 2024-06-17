b89dc20bec5cad19bb1bfabf6cf77117b47351a9
antrax <fredcox@gmail.com>
2024-06-17 08:06:39 -0300

    ok

--- 1-after-chroot.sh
+++ 1-after-chroot.sh
@@ -2,10 +2,14 @@
 
 source ./rainbow.sh
 
-echoyellow "Set timezone..."
+gA=$(echogreen "==>")
+
+echo "$gA Set timezone..."
 
 ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 hwclock --systohc
+
+echo "$gA Locale, hostname and hosts settings..."
 
 nvim /etc/locale.gen
 locale-gen
@@ -20,20 +24,22 @@ echo '127.0.0.1 localhost' >/etc/hosts
 echo '::1 localhost' >>/etc/hosts
 echo '127.0.1.1 shadowBox' >>/etc/hosts
 
-echoyellow 'Password for user root...'
+echo "$gA Password for user root..."
 passwd
 
-echoyellow 'Add user antrax...'
+echo "$gA Add user antrax..."
 useradd -m antrax
 usermod -aG wheel antrax
-echoyellow 'Password for user antrax...'
+echo "$gA Password for user antrax..."
 passwd antrax
 
 EDITOR=nvim visudo
 
-echoyellow 'Install grub...'
+echo "$gA Install grub..."
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
-echoyellow 'Enable Network Manager...'
+echo "$gA Enable Network Manager..."
 systemctl enable NetworkManager
+
+echo "$gA Everything OK!"
