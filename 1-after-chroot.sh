f36373dd55f90a4762e4d91da352c41004b90bf2
antrax <fredcox@gmail.com>
2024-06-23 19:26:47 -0300

    ok

--- 1-after-chroot.sh
+++ 1-after-chroot.sh
@@ -22,7 +22,7 @@ echo 'shadowBox' >/etc/hostname
 
 echo '127.0.0.1 localhost' >/etc/hosts
 echo '::1 localhost' >>/etc/hosts
-echo '127.0.1.1 shadowBox' >>/etc/hosts
+echo '127.0.0.1 shadowBox' >>/etc/hosts
 
 echo "$gA Password for user root..."
 passwd
